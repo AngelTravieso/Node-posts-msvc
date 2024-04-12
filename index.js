dfcb50fc1dcf81e2003a967041ffd40e285a9f4a
AngelTravieso <angeltraviesoc@gmail.com>
2024-04-12 10:56:02 -0400

    Post Service Creation
    Initial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json())
+
+const posts = {};
+
+app.get('/post', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/post', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    // lakjdlaksjdlkas
+    const { title } = req.body;
+    
+    posts[id] = {
+        id, title
+    }
+
+    res.status(201).send(posts[id]);
+
+});
+
+app.listen(4000, () => {
+    console.log('Server listening on port 4000');
+})
